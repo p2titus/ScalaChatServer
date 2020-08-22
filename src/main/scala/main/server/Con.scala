b7c6dcbd6a7698aa59657e9743ee8ce75a2399cb
Mike <michael.hallam3@gmail.com>
2020-08-22 11:08:14 +0100

    Added con class to allow for centralised retrieval of the socket

---
+++ src/main/scala/main/server/Con.scala
@@ -0,0 +1,12 @@
+package server
+
+import java.net._
+import java.io._
+
+// handles returning the socket with the connection to 
+object Con {
+  private val ip: String = "127.0.0.1"
+  private val port: Int = 43225
+  def getSock: Socket = new Socket(getIP,getPort)
+  def closeSock(x: Socket): Unit = x.close
+}
