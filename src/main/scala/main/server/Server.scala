7779a9ccdf59456b3a1baf10d843b585eef9592f
Mike <michael.hallam3@gmail.com>
2020-08-22 11:25:39 +0100

    Added recv method in object Recv

--- src/main/scala/main/server/Server.scala
+++ src/main/scala/main/server/Server.scala
@@ -3,6 +3,7 @@ package server
 import common._
 import scala.concurrent._
 import scala.collection.immutable.Queue
+import 
 
 object Server {
   
@@ -11,13 +12,22 @@ object Server {
 // by using an immutable queue, side effects are limited to this object
 private object Recv[A] extends Thread {
   private var curQ: Queue[A] = new Queue[A]()
+
   def getQueue: Queue[A] = curQ
 
-  override def run(): Unit = recv
+  override def run(): Unit = {
+    val sock: Socket = Con.getSock
+    recv(sock)
+    Con.closeSock(sock)
+  }
 
-  def recv: Unit = {
+  // modifies curQ
+  def recv[A](skt: Socket): Unit = {
+    val inStream: InputStream = skt.getInputStream
+    val in: ObjectInputStream = new ObjectInputStream(inStream)
     while(true) {
-
+      val x: A = in.readObject // TODO - does this block until it's read?
+      this.curQ = this.curQ.enqueue(x)
     }
   }
 }
