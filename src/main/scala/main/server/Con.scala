5fb71482e8d850c9aea789ddc8559482d5b5647a
Mike <michael.hallam3@gmail.com>
2020-08-23 09:23:10 +0100

    Restructured Con to be more general and to allow for repeated creation of Socket

--- src/main/scala/main/server/Con.scala
+++ src/main/scala/main/server/Con.scala
@@ -6,10 +6,9 @@ import java.io._
 // handles returning the socket with the connection to 
 object Con {
   private val port: Int = 43225
-  def getSock: (ServerSocket,Socket) = {
-    val ss: ServerSocket = new ServerSocket(port)
-    val sock: Socket = ss.accept()
-    (ss,sock)
-  }
-  def closeSock(x: ServerSocket, y: Socket): Unit = { y.close; x.close }
+  def getSock(ss: ServerSocket): Socket = ss.accept
+
+  def getServerSock: ServerSocket = new ServerSocket(port)
+
+  def close[A <% Closeable](x: A): Unit = x.close
 }
